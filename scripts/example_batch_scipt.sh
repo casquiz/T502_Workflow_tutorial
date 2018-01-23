72655ea8a83e8d672d70555fe181c0eacb20bd53
rtraborn <rtraborn@indiana.edu>
2018-01-23 15:51:30 -0500

    Added example batch script

---
+++ scripts/example_batch_scipt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#PBS -N Example_job_script_T502
+#PBS -k o
+#PBS -q debug
+#PBS -l nodes=1:ppn=16,vmem=16gb
+#PBS -l walltime=1:00:00
+#BS -m abe
+#PBS -M rtraborn@indiana.edu
+
+module load sra-toolkit
+module load fastqc
+
+WD=/N/u/rtraborn/Carbonate/
+
+cd $WD
+
+echo "Starting job"
+
+echo "Downloading the files"
+
+fastq-dump SRR2078285
+fastq-dump SRR2078286
+
+echo "Running fastqc on the files"
+
+fastqc SRR2078285.fastq
+fastqc SRR2078286.fastq
